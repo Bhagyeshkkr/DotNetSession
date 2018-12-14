04ef1ba00ff9a8023c4c18f1277e065d418320d6
Bhagyesh Chinchole <bhagyeshc@ansh.com>
2018-12-14 17:37:21 +0530

    first commit for dotnet assignment

---
+++ Assessment1/Assessment1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Assessment1
+{
+    // Base class
+    class Person
+    {
+        // data members 
+        public string name;
+        public int age;   
+    }
+    // inheriting the Person class using :
+    class Employee : Person
+    {
+        // constructor of derived class 
+        public Employee()
+        {
+            Console.WriteLine("Derived Constructor");
+        }
+        public void readers(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            Console.WriteLine("Myself: " + name);
+            Console.WriteLine("My age is: " + age);
+        }
+
+    }
+
+    // Driver class 
+    class Driver
+    {
+        // Main Method 
+        static void Main(string[] args)
+        {
+
+            // creating object of derived class 
+              Employee g = new Employee();
+
+            // calling the method of base class 
+            // using the derived class object 
+               g.readers("Jacob", 24);
+            Employee e = new Employee();
+            // calling the method of derived class 
+            e.readers("Rajesh", 25);
+        }
+    }
+}
---
+++ Assessment1/Problem2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Calculator
+    {
+
+        public virtual int calc(int a, int b)
+        {
+            Console.WriteLine("Base function called");
+            return (a + b);
+        }
+    }
+
+    class Dcalculator : Calculator
+    {
+
+        public override int calc(int a, int b)
+        {
+            Console.WriteLine("Override function called");
+
+            return (a * b);
+        }
+    }
+
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            Calculator a = new Calculator();
+            int result = a.calc(10, 20);
+            Console.WriteLine(result);
+            
+            Calculator d = new Dcalculator();
+             result = d.calc(10, 20);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
